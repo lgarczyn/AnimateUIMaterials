f132daacee630ec01fe4ec806b16e15d30572a11
Lou Garczynski <lgarczyn@student.42.fr>
2025-06-09 01:28:47 +0200

    Fixed mask issues by checking if bufferedMaterial changed source Avoid any possible memory issues by tweaking memory flags for Materials Smarter name creation for new materials, includes mask info

--- Unity/Animate UI Material/Assets/Plugins/Animate UI Materials/BufferedMaterialModifier.cs
+++ Unity/Animate UI Material/Assets/Plugins/Animate UI Materials/BufferedMaterialModifier.cs
@@ -9,10 +9,17 @@ namespace Plugins.Animate_UI_Materials
   /// </summary>
   public abstract class BufferedMaterialModifier : MonoBehaviour, IMaterialModifier
   {
+    private static readonly int Stencil = Shader.PropertyToID("_Stencil");
+
     /// <summary>
     /// Hold the last modified material, to be re-used if possible
     /// </summary>
     Material _bufferedMaterial;
+
+    /// <summary>
+    /// Holds the base material used to create _bufferedMaterial
+    /// </summary>
+    Material _bufferedMaterialSource;
 
     /// <summary>
     /// From IMaterialModifier
@@ -26,26 +33,70 @@ namespace Plugins.Animate_UI_Materials
       // Return the base material if invalid or if this component is disabled
       if (!enabled || baseMaterial == null) return baseMaterial;
 
-      if (!_bufferedMaterial || _bufferedMaterial.shader != baseMaterial.shader)
+      if (!_bufferedMaterial || _bufferedMaterial.shader != baseMaterial.shader || baseMaterial != _bufferedMaterialSource)
       {
         DestroyBuffer();
 
         // Create a child material of the original
-        Material modifiedMaterial = new(baseMaterial.shader)
-        {
-          // Set a new name, to warn about editor modifications
-          name = $"{baseMaterial.name} OVERRIDE",
-          hideFlags = HideFlags.HideAndDontSave & HideFlags.NotEditable,
-        };
-#if UNITY_2022_1_OR_NEWER && UNITY_EDITOR
-        modifiedMaterial.parent = baseMaterial;
-#endif
-        _bufferedMaterial = modifiedMaterial;
+        _bufferedMaterial = CreateNewMaterial(baseMaterial, "OVERRIDE");
+        _bufferedMaterialSource = baseMaterial;
       }
 
       _bufferedMaterial.CopyPropertiesFromMaterial(baseMaterial);
       ModifyMaterial(_bufferedMaterial);
       return _bufferedMaterial;
+    }
+
+    private int? GetStencilId(Material baseMaterial)
+    {
+      if (baseMaterial == null) return null;
+
+      // Check if material has stencil prop to avoid warning
+      if (!baseMaterial.HasInt(Stencil)) return null;
+      int id = baseMaterial.GetInt(Stencil);
+      return id > 0 ? id : null;
+    }
+
+    /// <summary>
+    /// Create a new material variant of the base material.
+    /// Tries to set parent value from the source material for prettier editing.
+    /// Sets flags to avoid saving the material in assets.
+    /// Used for creating new buffered material or for the fake editor screen.
+    /// </summary>
+    /// <param name="baseMaterial">IMaterialModifier argument</param>
+    /// <param name="suffix">Suffix to append to the original material name</param>
+    /// <returns></returns>
+    private Material CreateNewMaterial(Material baseMaterial, string suffix)
+    {
+      Material realSource;
+      // Try to retrieve real base Material
+      if (TryGetComponent(out Graphic graphic))
+      {
+        realSource = graphic.material ? graphic.material : Canvas.GetDefaultCanvasMaterial();
+      }
+      else
+      {
+        Debug.LogWarning("No graphic found");
+        realSource = baseMaterial;
+      }
+
+      // Add mask info to the Material
+      if (GetStencilId(baseMaterial) is {} stencilId)
+      {
+        suffix = $"{suffix} MASKED {stencilId}";
+      }
+
+      Material modifiedMaterial = new (baseMaterial.shader)
+      {
+        // Set a new name, to warn about editor modifications
+        name = $"{realSource.name} {suffix}",
+        hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor,
+      };
+      // Set parent if supported
+#if UNITY_2022_1_OR_NEWER && UNITY_EDITOR
+      modifiedMaterial.parent = realSource;
+#endif
+      return modifiedMaterial;
     }
 
     void DestroyBuffer()
@@ -71,15 +122,7 @@ namespace Plugins.Animate_UI_Materials
     public Material GetEditorMaterial(Material baseMaterial)
     {
       // Create a child material of the original
-      Material modifiedMaterial = new(baseMaterial.shader)
-      {
-        // Set a new name, to warn about editor modifications
-        name = $"{baseMaterial.name} EDITOR",
-        hideFlags = HideFlags.HideAndDontSave,
-      };
-#if UNITY_2022_1_OR_NEWER && UNITY_EDITOR
-      modifiedMaterial.parent = baseMaterial;
-#endif
+      Material modifiedMaterial = CreateNewMaterial(baseMaterial, "EDITOR");
       modifiedMaterial.CopyPropertiesFromMaterial(baseMaterial);
       ModifyMaterial(modifiedMaterial);
       return modifiedMaterial;
